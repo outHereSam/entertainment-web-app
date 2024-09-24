64a26e50ec95c6f9e1b24534c5dcabceb359299f
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-24 15:29:14 +0000

    Sets up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./pages/signup/signup.component').then((m) => m.SignupComponent),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/media-list/media-list.component').then(
+        (m) => m.MediaListComponent
+      ),
+  },
+  {
+    path: ':category',
+    loadComponent: () =>
+      import('./pages/media-list/media-list.component').then(
+        (m) => m.MediaListComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/media-list/media-list.component').then(
+        (m) => m.MediaListComponent
+      ),
+  },
+];
---
+++ src/app/pages/media-list/media-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-list.component.html',
+  styleUrl: './media-list.component.sass'
+})
+export class MediaListComponent {
+
+}
