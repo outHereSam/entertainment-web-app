dba1c2736dc79c61d2c8fdc005683bb3656c3408
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-27 15:19:16 +0000

    Adds validation for signup form

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.sass',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    },
+    { validators: this.checkPasswordsMatch }
+  );
+
+  checkPasswordsMatch(control: AbstractControl) {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    if (password !== confirmPassword) {
+      return { notMatch: true };
+    }
+    return null;
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+    } else {
+      console.log(this.signupForm.errors);
+      this.signupForm.markAllAsTouched();
+    }
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  loginUser() {}
+
+  signupUser() {}
+
+  getAuthToken() {}
+}
