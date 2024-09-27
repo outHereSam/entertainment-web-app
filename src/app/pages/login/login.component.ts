c33fe862aef84115a355b6f2d288471cb987450e
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-27 15:24:18 +0000

    Adds validations for login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  signInForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  onSubmit() {
+    if (this.signInForm.valid) {
+      console.log(this.signInForm.value);
+    } else {
+      console.log(this.signInForm.errors);
+      this.signInForm.markAllAsTouched();
+    }
+  }
+}
